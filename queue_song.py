148d7405eeec41f7d886a77fcc8f1540db6a9f15
DEMNERD <45518657+DEMNERD@users.noreply.github.com>
2020-10-02 15:22:03 +0300

    Initial commit

--- queue_song.py
+++ queue_song.py
@@ -1,8 +1,8 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 from get_pl import *
-from guest_tokens_generator import generate_tokens
 
+group_name = ""
 headers = CaseInsensitiveDict()
 headers["authority"] = "api.spotifybuddy.com"
 headers["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
@@ -13,7 +13,7 @@ headers["origin"] = "https://spotifybuddy.com"
 headers["sec-fetch-site"] = "same-site"
 headers["sec-fetch-mode"] = "cors"
 headers["sec-fetch-dest"] = "empty"
-headers["referer"] = "https://spotifybuddy.com/group/lynnelees-music"
+headers["referer"] = f"https://spotifybuddy.com/group/{group_name}"
 playlist_obj = PlaylistObject()  # Declaring instance
 playlist_obj.from_id(playlist_id='4IifzqaU0gqSQ89yzZXu2K')
 url = "https://api.spotifybuddy.com/user/guestSession"
@@ -25,14 +25,13 @@ session.headers = headers
 
 url = "https://api.spotifybuddy.com/user/getSession"
 print(f'"{tokens}"')
-a = session.get(url, params={"link": "lynnelees-music_group", "workspace_userid": "guest",
+a = session.get(url, params={"link": group_name, "workspace_userid": "guest",
                              "tokens": f'"{tokens}"'})
 print(session.cookies)
 print(a.cookies)
 url = 'https://api.spotifybuddy.com/queueSong'
 for i in playlist_obj.tracks_list:
     data = '{"workspace_userid":"guest","uri":"spotify:track:%s",' \
-           '"uniqueid_type":"lynnelees-music_group","link":"lynnelees-music_group", "tokens":%s}' % (i.id,
-                                                                                                     tokens)
+           '"uniqueid_type":"lynnelees-music_group","link":%s, "tokens":%s}' % (group_name, i.id, tokens)
     resp = requests.post(url, headers=headers, data=data, cookies={'tokens': tokens})
     print(resp.content)
